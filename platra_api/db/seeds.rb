ddfb9e3d5954e307ffb40c3b407504c4ee77c143
mgtb1994 <maximilian.griesser@student.uibk.ac.at>
2018-01-21 19:23:53 +0100

    renamed password

--- platra_api/db/seeds.rb
+++ platra_api/db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-users = User.create(name: 'Max Muster', email: 'max.muster@uibk.at', password: '1234')
+users = User.create(name: 'Max Muster', email: 'max.muster@uibk.at', password_digest: '1234')
 
