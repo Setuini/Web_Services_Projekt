3985acb6cf37b3fcf409fdbaac3f80488d17f472
mip <mitterer.p@gmail.com>
2018-01-23 23:04:45 +0100

    Progress on timetable creation

--- platra_api/app/controllers/api_events_controller.rb
+++ platra_api/app/controllers/api_events_controller.rb
@@ -3,7 +3,7 @@
 # cafe, church, hair_care, movie_theater, museum, night_club, park, shopping_mall, zoo
 
 # relevant information per place
-# location (lat, long), name, rating, photos, opening_hours, phone_number, outdoors, place_id
+# location (lat, lng), name, rating, photos, opening_hours, phone_number, outdoors, place_id
 
 # time-based context
 # breakfast 8:00 - 10:00
@@ -32,9 +32,14 @@ class ApiEventsController < ApplicationController
 
     def initialize
         #@places_api_key = 'AIzaSyDEiC_i0rhwIaKQDM2X2Pm3OJW3A30-SVY'
-        #@places_api_key = 'AIzaSyAdcW1tyOj-ZYZTkfjtFRrvPnuTE-oR7os'
-        @places_api_key = 'AIzaSyAA7oeWdNhvVCIIk7SQE3IWKON7z4tA5Rg'
+        @places_api_key = 'AIzaSyAdcW1tyOj-ZYZTkfjtFRrvPnuTE-oR7os'
+        #@places_api_key = 'AIzaSyAA7oeWdNhvVCIIk7SQE3IWKON7z4tA5Rg'
+
         @weather_api_key = 'e4b258a5707a4e9689e7f994287949a1'
+
+        @types = ["spa", "casino", "amusement_park", "beauty_salon", "aquarium", "art_gallery", "bowling_alley", "book_store", "museum", "park", "shopping_mall", "zoo"]
+        @outdoor_types = ["park", "zoo"]
+        @evening_types = ["night_club", "bar", "movie_theater"]
 
         @places = GooglePlaces::Client.new(@places_api_key)
     end
@@ -52,51 +57,86 @@ class ApiEventsController < ApplicationController
     def get_location(city)
         location = JSON.parse open("https://maps.googleapis.com/maps/api/geocode/json?address=#{city}&key=#{@places_api_key}").read
         lat = location['results'][0]['geometry']['location']['lat']
-        long = location['results'][0]['geometry']['location']['lng']
-        return {"lat" => lat, "long" => long}
+        lng = location['results'][0]['geometry']['location']['lng']
+        return {"lat" => lat, "lng" => lng}
     end
 
-    def get_poi(city, lat="", long="")
+    def get_poi(city, types, excludes = [], lat="", lng="")
         if !city.empty?
             location = get_location(city)
             lat = location["lat"]
-            long = location["long"]
+            lng = location["lng"]
         end
 
-        #response = JSON.parse open("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{long}&type=park&rankby=distance&key=#{@places_api_key}").read
+        #response = JSON.parse open("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{lat},#{lng}&type=park&rankby=distance&key=#{@places_api_key}").read
 
-        return @places.spots(lat, long, :types => ['park', 'museum'])
+        return @places.spots(lat, lng, :types => types, :excludes => excludes)
     end
 
     def get_poi_details(place_id)
-        response = JSON.parse open("https://maps.googleapis.com/maps/api/place/details/json?placeid=#{place_id}&key=#{@places_api_key}").read 
-        return response 
+        return @places.spot(place_id)
+    end
+
+    def get_poi_photos(place_id)
+        spot = @places.spot(place_id)
+        photos = Array.new
+        length = spot.photos.length-1
+        if length > 3 
+           length = 3 
+        end 
+
+        for i in 0..length
+            photos[i] = spot.photos[i].fetch_url(800)
+        end
+
+        return photos
+    end
+
+    def get_distance(origins, destinations)
+        distance = JSON.parse open("https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins=#{origins}&destinations=#{destinations}&key=#{@places_api_key}").read
+    end
+
+    def create_poi_object(poi)
+        poi_details = get_poi_details(poi["place_id"])
+        poi_object = Hash.new
+        poi_object["place_id"] = poi["place_id"]
+        poi_object["lat"] = poi["lat"]
+        poi_object["lng"] = poi["lng"]
+        poi_object["name"] = poi["name"]
+        poi_object["rating"] = poi["rating"]
+        poi_object["photos"] = get_poi_photos(poi["place_id"])
+        poi_object["opening_hours"] = poi_details["opening_hours"] 
+        poi_object["phone_number"] = poi_details["international_phone_number"]
+        poi_object["outdoors"] = false 
+
+        return poi_object
     end
 
     def show
         logger.debug params
     
         from = Date.new(2018, 1, 23)
-        to = Date.new(2018, 1, 28)
+        to = Date.new(2018, 1, 25)
         duration = to - from
 
         timetable = Hash.new
         for i in 0..duration
             day = Hash.new
-            #poi 
-            day["breakfast"] = ""
-            day["morning_activity"] = ""
+            activities = get_poi("Innsbruck", [@types[rand(@types.length)] ,@types[rand(@types.length)], @types[rand(@types.length)]])
+
+            evening_activities = get_poi("Innsbruck", @evening_types)
+
+            #logger.debug types.length)
+
+            day["breakfast"] = "" #create_poi_object(get_poi("Innsbruck", ["restaurant", "cafe"])[0])
+            day["morning_activity"] = create_poi_object(activities[rand(activities.length)]) 
             day["lunch"] = ""
-            day["afternoon_activity"] = ""
+            day["afternoon_activity"] = create_poi_object(activities[rand(activities.length)]) 
             day["dinner"] = ""
-            day["evening_activity"] = ""
+            day["evening_activity"] = create_poi_object(evening_activities[rand(activities.length)]) 
             timetable[(from + i).to_s] = day
         end
 
-
-        events = get_poi("", 47.2718972802915, 11.4073689302915)
-        #forecast = weather("Innsbruck", Date.new(2018, 2, 7))
-        render json: events 
-        #render json: timetable
+        render json: timetable 
     end
 end
