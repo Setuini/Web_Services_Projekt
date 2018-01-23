4a2dca81499847986c58f63ebab7d1c51fc361bb
mgtb1994 <maximilian.griesser@student.uibk.ac.at>
2018-01-23 21:21:59 +0100

    added some functions for timetable

--- platra_api/app/controllers/time_table_controller.rb
+++ platra_api/app/controllers/time_table_controller.rb
@@ -9,15 +9,60 @@ class TimeTableController < ApplicationController
 
   #here a new timetable is created
   def saveTimeTable
-
+    msg = [];
+    if !(TimeTable.exists?(name: params[:name], user_id: current_user.id))
+      timetable = TimeTabele.new(:user_id => current_user.id, :name => params[:name], :location => params[:location]);
+      timetable.save
+      msg.push('TimeTable created successfully');
+      render json: { msg: msg.map(&:inspect).join(', ') }, status: 201
+    else
+      msg.push('TimeTable not created');
+      render json: { msg: msg.map(&:inspect).join(', ') }, status: 422
+    end
   end
 
-  #here the entries are saved
-  def saveEntry
-    if (TimeTable.exists?(id: params[:timeTableId], user_id: current_user.id))
-
+  #here pointofinterest is saved
+  def savePoI
+    msg = [];
+    if !(PointOfInterest.exists?(name: params[:name], longitude: params[:longitude], latitude: params[:latitude]))
+      poi = PointOfInterest.new(:name => params[:name], :longitude => params[:longitude], :latitude => params[:latitude], :params => params[:params]);
+      poi.save
+      msg.push('PointOfInterest created successfully');
+      render json: { msg: msg.map(&:inspect).join(', ') }, status: 201
     else
+      msg.push('PointOfInterest not created');
+      render json: { msg: msg.map(&:inspect).join(', ') }, status: 422
+    end
+  end
 
+  #if entry is older than 2 weeks, delete all poi of this location
+  def isOlderTwoWeeks(name)
+    if PointOfInterest.exists?(name: name)
+      poi = PointOfInterest.find_by location: name
+      time = DateTime.now.to_i - poi.created_at.to_i
+      if time > 60*60*24*14
+        PointOfInterest.where(:location => "Innsbruck").destroy_all
+        true
+      else
+        false
+      end
+    else
+      true
+    end
+  end
+
+  def timeTableEntry
+    msg = [];
+    if !(PointOfInterest.exists?(name: params[:nameP], longitude: params[:longitude], latitude: params[:latitude])) || !(TimeTable.exists?(name: params[:nameT], user_id: current_user.id))
+      table = TimeTable.find_by name: params[:nameT], user_id: current_user.id;
+      poi = PointOfInterest.find_by name: params[:name], longitude: params[:longitude], latitude: params[:latitude]
+      td = TimeTableEntry.new(:point_of_interest_id => poi.id,time_table_id => table.id,:begin => params[:begin], :end => params[:end]);
+      td.save
+      msg.push('PointOfInterest created successfully');
+      render json: { msg: msg.map(&:inspect).join(', ') }, status: 201
+    else
+      msg.push('TimeTableEntry not created');
+      render json: { msg: msg.map(&:inspect).join(', ') }, status: 422
     end
   end
 end
