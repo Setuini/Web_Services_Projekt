6c49a2b8552bd2015ad7c752eea28e9339450b26
mgtb1994 <maximilian.griesser@student.uibk.ac.at>
2018-01-25 17:21:40 +0100

    added delete and change for timetable

--- platra_api/app/controllers/time_table_controller.rb
+++ platra_api/app/controllers/time_table_controller.rb
@@ -34,33 +34,18 @@ class TimeTableController < ApplicationController
     end
   end
 
-  #here pointofinterest is saved
-  def savePoI
-    msg = [];
-    if !(PointOfInterest.exists?(place_id: params[:place_id]))
-      poi = PointOfInterest.new(:location => params[:location],:name => params[:name], :longitude => params[:longitude], :latitude => params[:latitude], :params => params[:params], :types =>[:types], place_id => params[:place_id]);
-      poi.save
-      msg.push('PointOfInterest created successfully');
+  def changeTimeTable
+    msg[];
+    if (TimeTable.exists?(name: params[:name], user_id: current_user.id))
+      timetable = TimeTable.find_by user_id: current_user.id, name: params[:name]
+      timetable.name = params[:name];
+      timetable.location = params[:location];
+      timetable.save;
+      msg.push('TimeTable changed successfully');
       render json: { msg: msg.map(&:inspect).join(', ') }, status: 201
     else
-      msg.push('PointOfInterest already created');
+      msg.push('TimeTable not created');
       render json: { msg: msg.map(&:inspect).join(', ') }, status: 422
-    end
-  end
-
-  #if entry is older than 2 weeks, delete all poi of this location
-  def isOlderTwoWeeks(name)
-    if PointOfInterest.exists?(name: name)
-      poi = PointOfInterest.find_by location: name
-      time = DateTime.now.to_i - poi.created_at.to_i
-      if time > 60*60*24*14
-        PointOfInterest.where(:location => "Innsbruck").destroy_all
-        true
-      else
-        false
-      end
-    else
-      true
     end
   end
 
@@ -76,6 +61,53 @@ class TimeTableController < ApplicationController
         render json: { msg: msg.map(&:inspect).join(', ') }, status: 201
       else
         msg.push('TimeTableEntry already there');
+        render json: { msg: msg.map(&:inspect).join(', ') }, status: 201
+      end
+    else
+      msg.push('TimeTable or PointOfInterest not created');
+      render json: { msg: msg.map(&:inspect).join(', ') }, status: 422
+    end
+  end
+
+  def deleteTimeTableEntry
+    msg = [];
+    if (PointOfInterest.exists?(place_id: params[:place_id])) && (TimeTable.exists?(name: params[:name], user_id: current_user.id))
+      table = TimeTable.find_by place_id: params[:name], user_id: current_user.id;
+      poi = PointOfInterest.find_by place_id: params[:place_id]
+      if TimeTableEntry.exists?(time_table_id: table.id, begin: params[:begin], end: params[:end])
+        td = TimeTableEntry.find_by time_table_id: table.id, begin: params[:begin], end: params[:end]
+        td.delete
+        msg.push('TimeTableEntry deleted successfully');
+        render json: { msg: msg.map(&:inspect).join(', ') }, status: 201
+      else
+        msg.push('TimeTableEntry not there');
+        render json: { msg: msg.map(&:inspect).join(', ') }, status: 201
+      end
+    else
+      msg.push('TimeTable or PointOfInterest not created');
+      render json: { msg: msg.map(&:inspect).join(', ') }, status: 422
+    end
+  end
+
+  def changeTimeTableEntry
+    msg = [];
+    if (PointOfInterest.exists?(place_id: params[:place_id])) && (TimeTable.exists?(name: params[:name], user_id: current_user.id))
+      table = TimeTable.find_by place_id: params[:name], user_id: current_user.id;
+      poi = PointOfInterest.find_by place_id: params[:place_id]
+      if TimeTableEntry.exists?(time_table_id: table.id, begin: params[:begin], end: params[:end])
+        td = TimeTableEntry.find_by time_table_id: table.id, begin: params[:begin], end: params[:end]
+
+        td.point_of_interest_id = poi.id;
+        td.begin = params[:new_begin];
+        td.end = params[:new_end];
+        td.types = params[:types];
+
+        td.save;
+
+        msg.push('TimeTableEntry changed successfully');
+        render json: { msg: msg.map(&:inspect).join(', ') }, status: 201
+      else
+        msg.push('TimeTableEntry not there');
         render json: { msg: msg.map(&:inspect).join(', ') }, status: 201
       end
     else
