2eb68bbbbe412c658d1eac1e2789b1a2e6ed9123
Setuini <Felix.Schett@student.uibk.ac.at>
2018-01-25 23:27:58 +0100

    import clean up

--- platra_web/src/components/Timetable.js
+++ platra_web/src/components/Timetable.js
@@ -1,9 +1,7 @@
 import React  from 'react';
-//import { Link } from 'react-router-dom'
 import moment from 'moment';
-import { TimetablePage } from './TimetablePage.js';
 import { TimetableDay } from './TimetableDay.js';
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row } from 'reactstrap';
 import OwlCarousel from 'react-owl-carousel';
 
 export class Timetable extends React.Component {
@@ -19,14 +17,6 @@ export class Timetable extends React.Component {
       errors: '',
       fetchInProgress: false
     };
-    /*
-    console.log(this.state);
-    console.log("Timetable - startDate: "+moment(this.state.startDate).format("DD/MM/YYYY"));
-    console.log("Timetable - endDate: "+moment(this.state.endDate).format("DD/MM/YYYY") );
-    */
-    //this.getDay = this.getDay.bind(this);
-    //this.saveTimetable = this.saveTimetable.bind(this);
-    console.log("Timetable - Location: "+this.state.location);
     this.nextPage = this.nextPage.bind(this);
     this.prevPage = this.prevPage.bind(this);
     this.hasNext = this.hasNext.bind(this);
@@ -118,32 +108,9 @@ export class Timetable extends React.Component {
 
   // create timetable according to days of the fetch
   render() {
-    //var len = 0;
-    //var numPages = 1;
-    //if(this.state.startDate !== undefined && this.state.endDate !== undefined){
-      //var a = this.state.startDate;
-      //var b = this.state.endDate;
-      //len = b.diff(a, 'd');
-    //}
-
-    //numPages = len/3;
-    //var pages=[];
-    //var start = this.state.startDate;
-    //var end = moment(start, 'DD/MM/YYYY').add(3, 'd');
-    //for (var i=0; i < numPages; i++){
-      //console.log("Create Page");
-      //pages.push(<TimetablePage key={i} pageNumber={i} prevPage={this.prevPage} nextPage={this.nextPage} hasNext={this.hasNext} hasPrev={this.hasPrev} start={start} end={end} location={this.state.location}/>);
-      //start = end;
-      //end = moment(start, 'DD/MM/YYYY').add(3, 'd');
-
-      //if (moment(end).isAfter(this.state.endDate)) {
-        //end = moment(this.state.endDate);
-      //}
-    //}
     var timetableDays = [];
     var date = moment(this.state.startDate);
     var len = moment(this.state.endDate).diff(date, 'd');
-    console.log(len);
     for (var i=0; i < len; i++) {
         date = moment(this.state.startDate).add(i,'d').format("DD/MM/YYYY");
         var day = this.getDay(moment(this.state.startDate).add(i,'days').day());
